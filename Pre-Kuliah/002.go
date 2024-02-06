9642cdd735b6ba43ebdc87eca16d97d32b437fe9
Habb <ihabhasanainakmal0409@gmail.com>
2024-02-07 00:48:31 +0700

    revisi 1

--- Pre-Kuliah/002.go
+++ Pre-Kuliah/002.go
@@ -1,31 +1,14 @@
+// Program NIM
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
+import "fmt"
 
 func main() {
-	var tahunMasuk, tanggalLahir int
-
-	// Membaca masukan tahun masuk dan tanggal lahir
-	fmt.Print("Masukkan tahun masuk kuliah (format YYYY): ")
-	fmt.Scan(&tahunMasuk)
-	fmt.Print("Masukkan tanggal lahir (format DDMMYYYY): ")
-	fmt.Scan(&tanggalLahir)
-
-	// Mendapatkan dua digit terakhir dari tahun masuk
-	aa := tahunMasuk % 100
-
-	// Mendapatkan dua digit terakhir dari tahun lahir
-	bb := (tanggalLahir % 10000) / 100
-
-	// Mendapatkan dua digit tanggal lahir
-	cc := tanggalLahir % 100
-
-	// Menggabungkan nilai aa, bb, dan cc untuk membentuk NIM
-	nim, _ := strconv.Atoi(fmt.Sprintf("1302%02d%02d%02d", aa, bb, cc))
-
-	// Menampilkan hasil NIM
-	fmt.Println("NIM Mahasiswa:", nim)
+	var tahun_masuk, tanggal_lahir, NIM, AA, BB, CC int
+	fmt.Scan(&tahun_masuk, &tanggal_lahir)
+	AA = (tahun_masuk % 100) * 10000
+	BB = (tanggal_lahir % 100) * 100
+	CC = tanggal_lahir / 1000000
+	NIM = 130000000 + AA + BB + CC
+	fmt.Println(NIM)
 }
--- Pre-Kuliah/tempCodeRunnerFile.go
+++ Pre-Kuliah/tempCodeRunnerFile.go
@@ -1,1 +1,22 @@
-a
+package main
+
+import "fmt"
+
+func main() {
+	var bilbul int
+	var kelipatan bool
+	stop := false
+	fmt.Scan(&bilbul)
+	for !stop {
+		bilbul = bilbul - 15
+		if bilbul == 0 {
+			kelipatan = true
+		}
+		if bilbul <= 0 {
+			stop = true
+		}
+	}
+
+	fmt.Println(kelipatan)
+
+}
