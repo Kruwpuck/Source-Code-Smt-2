6cd0d853b429c24475e13ea2a42149083b771b1a
Habb <ihabhasanainakmal0409@gmail.com>
2024-04-03 00:27:35 +0700

    na

--- coba.go
+++ coba.go
@@ -2,25 +2,98 @@ package main
 
 import "fmt"
 
-// Function to check if a number is perfect or not
-func isPerfectNumber(num int) bool {
-	sum := 0
-	for i := 1; i <= num/2; i++ {
-		if num%i == 0 {
-			sum += i
-		}
-	}
-	return sum == num
+// tipe bentukan struct waktu dengan field jam, menit, detik semuanya bertipe integer
+type waktu struct {
+	jam, menit, detik int
+}
+
+// tipe bentukan struct kendaraan dengan field jenis kendaraan (jke), nopol (no),
+// jam masuk (jm) dan jam keluar (jk) bertipe waktu
+// bea parkir (bea) dan durasi parkir (durasi) bertipe integer
+type kendaraan struct {
+	jke, no     string
+	jm, jk      waktu
+	bea, durasi int
 }
 
 func main() {
-	var num int
-	fmt.Print("Enter a number: ")
-	fmt.Scan(&num)
+	var k01, k02, k03 kendaraan
+	var jenis, nopol string
+	var j1, m1, d1, j2, m2, d2 int
 
-	if isPerfectNumber(num) {
-		fmt.Println(num, "is a perfect number.")
-	} else {
-		fmt.Println(num, "is not a perfect number.")
+	fmt.Scan(&jenis, &nopol, &j1, &m1, &d1, &j2, &m2, &d2)
+	// Membuat kendaraan k01
+	k01 = kendaraan_baru(jenis, nopol, j1, m1, d1, j2, m2, d2)
+
+	fmt.Scan(&jenis, &nopol, &j1, &m1, &d1, &j2, &m2, &d2)
+	// Membuat kendaraan k02
+	k02 = kendaraan_baru(jenis, nopol, j1, m1, d1, j2, m2, d2)
+
+	fmt.Scan(&jenis, &nopol, &j1, &m1, &d1, &j2, &m2, &d2)
+	// Membuat kendaraan k03
+	k03 = kendaraan_baru(jenis, nopol, j1, m1, d1, j2, m2, d2)
+
+	// hitung bea parkir 3 kendaraan
+	bea_parkir(&k01)
+	bea_parkir(&k02)
+	bea_parkir(&k03)
+	fmt.Println(k01.durasi)
+	// cetak data parkir per kendaraan
+	cetak_data(k01)
+	cetak_data(k02)
+	cetak_data(k03)
+}
+
+func kendaraan_baru(j, n string, a, b, c, d, e, f int) kendaraan {
+	/*  Mengembalikan nilai kendaraan dengan data jenis kendaraan (j), nopol (n),
+	jam masuk dalam jam (a), menit (b), detik (c) dan jam keluar dalam jam (d), menit (e), detik (f).
+	*/
+	var k kendaraan
+	k.jke = j
+	k.no = n
+	k.jm.jam = a
+	k.jm.menit = b
+	k.jm.detik = c
+	k.jk.jam = d
+	k.jk.menit = e
+	k.jk.detik = f
+	return k
+}
+
+func durasi_parkir(k *kendaraan) {
+	/*  IS: k terdefinisi, kecuali durasi parkir (k.durasi) dan bea parkir (k.bea)
+	    Proses : Ubah durasi tot_detik menjadi k.durasi dalam satuan jam
+	    FS: Durasi parkir (k.durasi) berisi nilai dalam satuan jam
+	*/
+
+	var tot_detik int
+	tot_detik = (k.jk.jam*3600 + k.jk.menit*60 + k.jk.detik) - (k.jm.jam*3600 + k.jm.menit*60 + k.jm.detik)
+	k.durasi = tot_detik / 3600
+	if tot_detik%3600 != 0 {
+		k.durasi += 1
 	}
+}
+
+func bea_parkir(k *kendaraan) {
+	/*  IS: k terdefinisi, kecuali durasi parkir (k.durasi) dan bea parkir (k.bea)
+	    Proses: Memanggil prosedur durasi_parkir dan menghitung k.bea
+		    Jika kendaraan mobil, biaya parkir jam pertama Rp 6000, per jam selanjutnya Rp 3000
+			Jika kendaraan motor, biaya parkir jam pertama Rp 2500, per jam selanjutnya Rp 1500
+	FS: Durasi parkir (k.durasi) berisi nilai dalam satuan jam
+	*/
+	durasi_parkir(k)
+	if k.jke == "mobil" {
+		k.bea = 6000 + 3000*(k.durasi-1)
+	} else if k.jke == "motor" {
+		k.bea = 2500 + 1500*(k.durasi-1)
+	}
+}
+
+func cetak_data(k kendaraan) {
+	/*
+		IS: k terdefinisi
+		FS: Tercetak data parkir kendaraan dengan format:
+			"Bea parkir <jke> nopol <no> dengan durasi <durasi> jam: Rp <bea>."
+	*/
+	fmt.Printf("Bea parkir %s nopol %s dengan durasi %d jam: Rp %d.\n", k.jke, k.no, k.durasi, k.bea)
 }
--- coba2.go
+++ coba2.go
@@ -2,13 +2,28 @@ package main
 
 import "fmt"
 
+type titik struct {
+	x, y float64
+}
+type lingkaran struct {
+	pusat                  titik
+	radius, luas, keliling float64
+}
+
 func main() {
-	var k1, k2, k10, masuk int
-	fmt.Scan(&masuk)
-	k10 = masuk / 10000
-	k2 = (masuk % 10000) / 2000
-	k1 = (masuk % 10000 % 2000) / 1000
-	fmt.Println(k10, "lembar 10000")
-	fmt.Println(k2, "lembar 2000")
-	fmt.Println(k1, "lembar 1000")
+	var t1, t2 lingkaran
+
+	fmt.Scan(&t1.pusat.x, &t1.pusat.y, &t1.radius)
+	fmt.Scan(&t2.pusat.x, &t2.pusat.y, &t2.radius)
+	lukel(&t1)
+	lukel(&t2)
+	cetak(t1)
+	cetak(t2)
+}
+func lukel(k *lingkaran) {
+	k.luas = 3.14 * (k.radius * k.radius)
+	k.keliling = 2.0 * 3.14 * k.radius
+}
+func cetak(k lingkaran) {
+	fmt.Printf("Lingkaran berpusat di titik (%v, %v) dan berradius %.2f memiliki luas %.2f dan keliling sebesar %.2f\n", k.pusat.x, k.pusat.y, k.radius, k.luas, k.keliling)
 }
